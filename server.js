0ed27a3b1ec2040aeb082b95fdebc8eb88426df8
rodrigo@rtoledo.inf.br <rodrigo@rtoledo.inf.br>
2020-03-03 13:52:57 -0300

    force remote mongodb

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ const requireDir = require('require-dir');
 const app = express();
 app.use(express.json());
 
-mongoose.connect('mongodb://localhost:27017/filoapi', { useNewUrlParser: true, useUnifiedTopology: true });
+// const mongourl = __DEV__ ? 'mongodb://localhost:27017/filoapi' : 'mongodb://heroku_9b647v1f:3vgtlrl19vpf02nbmoj3lb697c@ds353338.mlab.com:53338/heroku_9b647v1f';
+const mongourl = 'mongodb://heroku_9b647v1f:3vgtlrl19vpf02nbmoj3lb697c@ds353338.mlab.com:53338/heroku_9b647v1f';
+
+mongoose.connect(mongourl, { useNewUrlParser: true, useUnifiedTopology: true });
 
 requireDir('./src/models/');
 
